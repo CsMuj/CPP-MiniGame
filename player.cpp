36bfa7a5425a409136926bf1053555056503982a
CsMuj <Mujtaba.Chowdhury@csedge.org>
2023-01-24 16:39:52 -0500

    Push-1

---
+++ player.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <iostream>
+
+// Player Class script
+
+int main() {
+    
+    return 0;
+}
+
+
+
+// Tests
+// int main() {
+//     std::string name;
+//     std::cout << "enter";
+//     std::cin  >> name;
+//     std::cout << "Hello World" << std::endl << name << std::endl;
+    
+//     int value = 1;
+//     bool statment = false;
+
+//     bool tests[5] = {true, false, true, true, false};
+
+//     std::cout << tests[4];
+
+//     std::cout << tests;
+//     std::cout << tests+1;
+
+//     return 0;
+// }
