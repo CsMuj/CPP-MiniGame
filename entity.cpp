353b1e43df0a792e2bac2bb1eb8b4252b6c9ed41
CsMuj <Mujtaba.Chowdhury@csedge.org>
2023-02-06 18:00:50 -0500

    day-end

--- entity.cpp
+++ entity.cpp
@@ -11,10 +11,22 @@ class entity {
         int luckPoints = 1;
         int actionPoints = 1;
         std::vector<std::string> special_skills = {};
+
+        entity(std::string name, int hitpoints, defencePoints, attackPoints, luckPoints, actionsPoints) {
+            name = name;
+            hitpoints = hitpoints
+        }
 };
+
 class player : entity {
     public:
         int staminaPoints = 10;
         int level = 1;
+
+        player(int stamina, int level, std::string name, int hitpoints) 
+        : entity(name, hitpoints) {
+            staminaPoints = stamina;
+            level = level;
+        }
     
 };
--- main.cpp
+++ main.cpp
@@ -1,18 +1,22 @@
 #include <string>
 #include <iostream>
 #include <time.h>
+#include "entity.cpp"
 
-void initalize() {
-
+int random(int x, int y) {
+    return (rand() % (y - x)) + x;
 }
 
 int main() {
-
     bool on = true;
-    
-    while (on) {
 
-    };
+    std::string name;
+    std::cin >> name;
+    player p = {name, random(10, 30), random(1,5), random(1,5), random(1,5), random(1,2)};
+    // while (on) {
+
+    std::cout << "My player's stamina is " << p.staminaPoints << "\n";
+    // };
 
     return 0;
 }
